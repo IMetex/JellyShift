a38806bfded5c03b462c7e36abd1ae66ef06f1ed
Baris Mete <brs.mete7@gmail.com>
2024-01-18 15:43:46 +0300

    refactoring

--- Assets/Scripts/JellyShift/Movement/ForwardMovement.cs
+++ Assets/Scripts/JellyShift/Movement/ForwardMovement.cs
@@ -1,34 +1,25 @@
 using System;
+using System.Collections;
 using JellyShift.Manager;
+using JellyShift.ScritableObject;
 using UnityEngine;
 
 namespace JellyShift.Movement
 {
     public class ForwardMovement : MonoBehaviour
     {
-        [SerializeField] private float forwardSpeed;
-        [SerializeField] private float speedIncreaseRate;
-        [SerializeField] private float maxForwardSpeed;
-
+        [SerializeField] private SpeedOS _speedOS;
         public bool isObstacle = false;
-
-
-        public float Speed
-        {
-            get => forwardSpeed;
-            set => forwardSpeed = value;
-        }
 
         private void Update()
         {
-            if (!GameManager.Instance.IsGameStarted)
+            if (!GameManager.Instance || !GameManager.Instance.IsGameStarted)
                 return;
 
             if (CanMove())
             {
-                forwardSpeed += speedIncreaseRate * Time.deltaTime;
-                forwardSpeed = Mathf.Clamp(forwardSpeed, 0f, maxForwardSpeed);
-                transform.position += -transform.forward * (forwardSpeed * Time.deltaTime);
+                _speedOS.SetForwardSpeed();
+                transform.position += -transform.forward * (_speedOS.ForwardSpeed * Time.deltaTime);
             }
         }
 
@@ -42,8 +33,30 @@ namespace JellyShift.Movement
             {
                 return !hit.collider.CompareTag("Obj");
             }
-            
+
             return true;
+        }
+
+        private void OnEnable()
+        {
+            StartCoroutine(nameof(Delay));
+        }
+
+        private IEnumerator Delay()
+        {
+            yield return new WaitForSeconds(0.01f);
+            GameManager.Instance.GameEnded += OnGameEnded;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.Instance.GameEnded -= OnGameEnded;
+        }
+
+        private void OnGameEnded()
+        {
+            if (_speedOS)
+                _speedOS.StartSpeedValue();
         }
     }
 }
---
+++ Assets/Scripts/JellyShift/ScritableObject/SpeedOS.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace JellyShift.ScritableObject
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "Speed", order = 0)]
+    public class SpeedOS : ScriptableObject
+    {
+        private float _forwardSpeed;
+        public float ForwardSpeed => _forwardSpeed;
+
+        [SerializeField] private float speedIncreaseRate;
+        [SerializeField] private float maxForwardSpeed;
+        [SerializeField] private float startForwardSpeed;
+        
+
+        public void SetForwardSpeed()
+        {
+            _forwardSpeed += speedIncreaseRate * Time.deltaTime;
+            _forwardSpeed = Mathf.Clamp(_forwardSpeed, 0f, maxForwardSpeed);
+        }
+
+        public void StartSpeedValue()
+        {
+            _forwardSpeed = startForwardSpeed;
+        }
+    }
+}
